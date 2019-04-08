262abf4b1763cfa7b3093a5bc5fdf0d9fdd17a44
cody-baker-1 <49406462+cody-baker-1@users.noreply.github.com>
2019-04-08 10:14:40 -0500

    Update lightbox.js

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -51,7 +51,7 @@
     positionFromTop: 50,
     resizeDuration: 700,
     showImageNumberLabel: true,
-    wrapAround: false,
+    wrapAround: true,
     disableScrolling: false,
     /*
     Sanitize Title
